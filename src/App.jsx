8b0912d3f9df3af5aadb23300db0f7fd28ce253f
coolrunner1 <153802775+coolrunner1@users.noreply.github.com>
2024-05-10 20:44:56 +0300

    Added my styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function Test(){
-  return(
-    <button onClick={buttonClick}>Hello world</button>
-  )
-}
-
 function App() {
   const [count, setCount] = useState(0)
 
+  const buttonClick=()=>{
+    setCount((count+1));
+  }
+
   return (
     <>
-      <Test />
+      <Button onClick={buttonClick} text="Hello world" />
+      <DisplayCnt count={count}/>
     </>
   )
 }
 
-const buttonClick=()=>{
-  console.log("Hello world")
+function DisplayCnt({count}){
+  return(
+    <div>{count}</div>
+  )
+}
+
+function Button(props){
+  return(
+    <button onClick={props.onClick}>{props.text}</button>
+  )
 }
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './styles.css'
+
+let hello=0;
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App counter={hello}/>
   </React.StrictMode>,
 )
+
